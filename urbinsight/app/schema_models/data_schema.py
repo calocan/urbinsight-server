0aa1ede2ef365f8ad3db4e3bd4f32463812f84ee
Andy Likuski <andy@likuski.org>
2018-06-30 11:02:34 -0600

    Debuging embedded data

--- urbinsight/app/schema_models/data_schema.py
+++ urbinsight/app/schema_models/data_schema.py
@@ -1,10 +1,12 @@
 import rescape_graphene.ramda as R
-from graphene import InputObjectType, InputField, ObjectType, DateTime, String, Float, Int, Mutation, Field, List
+from graphene import ObjectType, String, Float, List
 
 ###
 # Data fields are not a Django model, rather a json blob that is the field data of the Region and Resource models
 # So we list all the fields here and create a Graphene model RegionDataType and ResourceDataType
 ###
+from rescape_graphene import input_type_class
+from rescape_graphene.schema_helpers import CREATE
 
 region_data_fields = dict(
     example=dict(type=Float)
@@ -14,42 +16,69 @@ RegionDataType = type(
     'RegionDataType',
     (ObjectType,),
     R.map_with_obj(
-        # Here we use type_arg for List construction
-        lambda k, v: R.prop('type', v)(*R.compact([R.prop_or(None, 'type_arg', v)])),
+        # If we have a type_modifier function, pass the type to it, otherwise simply construct the type
+        lambda k, v: R.prop_or(lambda typ: typ(), 'type_modifier', v)(R.prop('type', v)),
         region_data_fields)
 )
 
 stage_data_fields = dict(
-    source=dict(type=String),
+    key=dict(type=String),
     name=dict(type=String),
-    targets=dict(type=List, type_arg=String)
+    targets=dict(type=String, type_modifier=lambda typ: List(typ))
 )
 
 StageDataType = type(
     'StageDataType',
     (ObjectType,),
     R.map_with_obj(
-        # Here we use type_arg for List construction
-        lambda k, v: R.prop('type', v)(*R.compact([R.prop_or(None, 'type_arg', v)])),
+        # If we have a type_modifier function, pass the type to it, otherwise simply construct the type
+        lambda k, v: R.prop_or(lambda typ: typ(), 'type_modifier', v)(R.prop('type', v)),
         stage_data_fields)
 )
 
-resource_data_fields = R.merge_dicts(
-    dict(
-        default_location=dict(type=List, type_arg=String),
-        columns=dict(type=List, type_arg=String),
-        stage_key=dict(type=String),
-        value_key=dict(type=String),
-        location_key=dict(type=String),
-        node_name_key=dict(type=String),
-        stages=dict(graphene_type=StageDataType, fields=stage_data_fields, type=List, type_arg=StageDataType)
+settings_data_fields = dict(
+    default_location=dict(type=String, type_modifier=lambda typ: List(String)),
+    columns=dict(type=String, type_modifier=lambda typ: List(typ)),
+    stage_key=dict(type=String),
+    value_key=dict(type=String),
+    location_key=dict(type=String),
+    node_name_key=dict(type=String),
+    raw_data=dict(
+        type=String,
+        type_modifier=lambda typ: List(String)
+    ),
+    stages=dict(
+        type=StageDataType,
+        graphene_type=StageDataType,
+        fields=stage_data_fields,
+        type_modifier=lambda typ: List(typ)
     )
+)
+
+SettingsDataType = type(
+    'SettingsDataType',
+    (ObjectType,),
+    R.map_with_obj(
+        # If we have a type_modifier function, pass the type to it, otherwise simply construct the type
+        lambda k, v: R.prop_or(lambda typ: typ(), 'type_modifier', v)(R.prop('type', v)),
+        settings_data_fields)
+)
+
+resource_data_fields = dict(
+    settings=dict(
+        type=SettingsDataType,
+        graphene_type=SettingsDataType,
+        fields=settings_data_fields,
+        type_modifier=lambda typ: List(typ)
+    ),
+    material=dict(type=String),
 )
 
 ResourceDataType = type(
     'ResourceDataType',
     (ObjectType,),
     R.map_with_obj(
-        # Here we use type_arg for List construction
-        lambda k, v: R.prop('type', v)(*R.compact([R.prop_or(None, 'type_arg', v)])),
-        resource_data_fields))
+        # If we have a type_modifier function, pass the type to it, otherwise simply construct the type
+        lambda k, v: R.prop_or(lambda typ: typ(), 'type_modifier', v)(R.prop('type', v)),
+        resource_data_fields)
+)
